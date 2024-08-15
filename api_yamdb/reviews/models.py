220243cd441dfc536eda6650a214a824cab64ab5
Sergey Krupnov <krupnovsergey@gmail.com>
2024-08-16 01:00:39 +0300

    модели, серриализаторы, вью, urls

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -4,3 +4,17 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     pass
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50, unique=True)
+
+class Genre(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50, unique=True)
+
+class Title(models.Model):
+    name = models.CharField(max_length=256)
+    year = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    genre = models.ManyToManyField(Genre)

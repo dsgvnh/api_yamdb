d82819d7a49bf8e34c0c0b2c67ca18b0715bd1fd
Denis <160116389+dsgvnh@users.noreply.github.com>
2024-09-02 01:21:17 +0300

    flake8

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -4,7 +4,6 @@ from django.core.validators import (
 )
 from django.core.exceptions import ValidationError
 from django.db import models
-from django.db.models import Avg
 from django.utils import timezone
 
 
@@ -103,12 +102,6 @@ def validate_year(value):
         raise ValidationError(
             f'Год не может быть больше текущего {current_year}'
         )
-
-
-def validate_year(value):
-    current_year = timezone.now().year
-    if value > current_year:
-        raise ValidationError(f'Год не может быть больше текущего {current_year}')
 
 
 class Title(models.Model):
